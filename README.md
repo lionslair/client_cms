08e44ff11966d85c6f0292891f46e686c36cd8af
Nathan Rzepecki <nathan@lionslair.net.au>
2014-10-26 14:29:56 +0800

    initial inport of the client cms drupal module

--- README.md
+++
@@ -1,4 +0,0 @@
-client_menu
-===========
-
-Creates a client menu system aimed at webshop building Drupal sites.
---
+++ client-cms-home.tpl.php
@@ -0,0 +1,5 @@
+<div id="client-cms-home">
+  <?php print variable_get('client_cms_home_text', 'fill in this content'); ?>
+  <p>Phone: <?php print variable_get('client_cms_webshop_phone', ''); ?></p>
+  <p>Email: <a href="mailto:<?php print variable_get('client_cms_webshop_email', 'email@domain.com'); ?>"><?php print variable_get('client_cms_webshop_email', 'email@domain.com'); ?></a></p>
+</div>
